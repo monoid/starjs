05972a45cf279b27e50578589dced852a2377163
Ivan Boldyrev <lispnik@gmail.com>
2010-02-10 19:00:18 +0600

    Fix numerous typos in Kepler modules.
    --HG--
    branch : google-compiler

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 ~/bin/google-compiler --js=lib/StarJs{.js,/Math.js,/Vector.js,/Time.js,/Coord.js,/Kepler.js,/Solar.js} --js_output_file=StarJs.min.js --compilation_level ADVANCED_OPTIMIZATIONS
+cat lib/StarJs{.js,/Math.js,/Vector.js,/Time.js,/Coord.js,/Kepler.js,/Solar.js} > StarJs.concat.js
