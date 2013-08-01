cf32829dcae254dfcd9f37ce65017a63003fa1fb
Ivan Boldyrev <lispnik@gmail.com>
2013-08-01 12:05:31 +0700

    Asm.js code for eccentric anomaly.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-~/bin/google-compiler --js=lib/StarJs{.js,/Math.js,/Vector.js,/Time.js,/Coord.js,/Kepler.js,/Solar.js} --js=lib/export.js --js_output_file=StarJs.min.js --compilation_level ADVANCED_OPTIMIZATIONS
+# ~/bin/google-compiler --js=lib/StarJs{.js,/Math.js,/Vector.js,/Time.js,/Coord.js,/Kepler.js,/Solar.js} --js=lib/export.js --js_output_file=StarJs.min.js --compilation_level ADVANCED_OPTIMIZATIONS
 cat lib/StarJs{.js,/Math.js,/Vector.js,/Time.js,/Coord.js,/Kepler.js,/Solar.js} > StarJs.debug.js
